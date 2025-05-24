820895ef0cb19ceb2a5e1bc43f8a0b3e0cb909f9
Ravi Kalia <kravi014@gmail.com>
2025-05-24 15:52:53 -0400

    final of lab1

--- JavaEx/src/App.jsx
+++ JavaEx/src/App.jsx
@@ -2,12 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Adult from './ArrayExFilter'
-import DisplayList from './ArrayExMap'
-import AgeStatus from './TernaryOperatorEx'
-import ReactEvent from './EventRectEx'
-import DisplayListstu from './StudentRec'
-import DisplayListstu2 from './StudentRd'
 import DisplayListstu3 from './FnStd'
 
 function App() {
@@ -16,31 +10,24 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <DisplayList />
-      <AgeStatus  />
-      <Adult  />
-      <ReactEvent />
-      <DisplayListstu />
-      <DisplayListstu2 />
+        <table>
+            <tr>
+              <th>Student Name :</th>
+              <td>Manisha Sharma</td>
+            </tr>
+           
+            <tr>
+              <th>Student ID :</th>
+              <td>N01718641</td>
+            </tr>
+             <tr>
+              <th>Course :</th>
+              <td>WDDM-131</td>
+            </tr>
+        </table>
+       </div>
+   
+      
       <DisplayListstu3 />
     </>
   )
